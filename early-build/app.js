941fc22ade3d53bdf7aa70642cb0fd4d39d7a4f7
Adrian Quiroz <adrianquiroz166@gmail.com>
2023-05-17 21:50:45 -0500

    Experimenting

--- early-build/app.js
+++
@@ -1,8 +0,0 @@
-var pokemon1 = document.getElementById("pokemon1");
-var printButton = document.getElementById("printBtn");
-var printValue = document.getElementById("enteredPokemon");
-function printEnteredValue() {
-    printValue.textContent = pokemon1.value.toString();
-    // printValue.textContent = pokemon1;
-}
-printButton.addEventListener("click", printEnteredValue);
--- early-build/main.py
+++
@@ -1,6 +0,0 @@
-import pokebase as pb
-chesto = pb.APIResource('berry', 'chesto')
-# chesto.name
-print(chesto.name)
-print(chesto.natural_gift_type.name)
-
