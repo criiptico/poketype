57836ef0d741e78f5ba1f6aef6347baaef5f43e8
Adrian Quiroz <adrianquiroz166@gmail.com>
2023-05-19 20:22:38 -0500

    I'm starting to map out the functions and what I'd need to do if- and only if the moves have a defining type for moves... ex. electric, water, fire, fairy...etc. If they're not, then, well, web scraper or.. something. I'm not sure what to do for that. If anything, try it with a smaller database but it's honestly not as fun. - Adrian Q/criiptico

--- prototype/main.py
+++ prototype/main.py
@@ -1,1 +1,21 @@
-print("Hello World!")
+# print("Hello World!")
+
+
+# Test code provided by AioPokeApi at https://beastmatser.github.io/aiopoke/
+# Running this code to test how it works.
+
+import asyncio
+import aiopoke
+
+
+async def main() -> None:
+    async with aiopoke.AiopokeClient() as client:
+        berry = await client.get_berry(1)
+        print(berry)
+
+        # An object can also have 'MinimalResources' as attributes
+        # You can fetch those with .fetch(), for example:
+        item = await berry.item.fetch()
+
+
+asyncio.run(main())
