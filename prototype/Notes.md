db2359ac376f33195f6fce446708da1cad193b94
Adrian Quiroz <adrianquiroz166@gmail.com>
2023-06-06 20:07:10 -0500

    Finalized move data retrieval and created a dump function to check the move data. - Adrian Q/criiptico

--- prototype/Notes.md
+++ prototype/Notes.md
@@ -1,4 +1,29 @@
 # Notes:
+
+##### 06/06/23
+	So, I've organized a way to input a pokemon's moves and created a way to dump all their moves.
+	The next steps are as shown below:
+		- Search moves' base damage | Make another dict with the id and the move's base damage?
+        - Develop type chart matrix, function, and txt file to read from.
+        - Search moves on type chart in comparison against opposing pokemon.
+            + sort move effectiveness in a max and min heap for each pokemon. (2 min and 2 max)
+
+
+##### 06/05/23
+	There are a couple of things that I could make. I could make a map to store
+	all of the relevant information regarding some pokemon. In addition, I could also 
+	use a max and a min heap to filter the max and min damage
+	of each move to the pokemon against each of them.
+		- General database <- where everything is stored
+		- It'd make sense to store the id's instead of all the dadta, faster? 
+			+ max damage database <- where id's are stored and they're ordered from max to min
+			+ min damage database <- where id's are stored and they're ordered from in to max
+
+##### 06/04/23
+	Success! I've parsed the string up to the move name. There's two things that
+	I need to do now. I need to make a hash map or a map or sorts to 
+	insert the id and the name of the move onto with the key.
+
 ##### 05/31/23
 	As of now, I've discovered that 
 	(- now that I think about it, I should consider using jupyter notebooks, 
--- prototype/main.py
+++ prototype/main.py
@@ -6,7 +6,6 @@
 
 import asyncio
 import aiopoke
-import json
 
 # Below is test code
 # async def main() -> None:
@@ -24,34 +23,83 @@ import json
     
 
 # The input should be everything after PokemonMove and up to the next PokemonMove or a \n
-def loadMoves(pokemon, moves, moveSet): # What data type is moves??? Is moves a list?
+def loadMoves(pokemon, moves, moveMap): # What data type is moves??? Is moves a list?
     count = 0
     for i in moves: 
         currentMove = str(i)
         moveIt = str(i).find(',')
-        findMoves(str(i), ',', 3)
+        findMoves(str(i), ',', 3, moveMap)
         # print(str(i))
+    return moveMap
 
-def findMoves(s, toFind, n):
+
+def findMoves(s, toFind, n, moveMap):
     itFind = s.find(toFind)
     itName = s.find("name=") + 6
+    # moveMap = {}
     i = 0
     for i in range(n):
         itFind = s.find(toFind, itFind + 1)
-    print(s[itName:itFind - 2])
+    moveName = s[itName:itFind - 2]
+    moveMap[findId(s)] = moveName
+    # print(s[itName:itFind - 2])
+    # moveSet[] = moveName
+    return moveMap
+
+def findId(s):
+    itFind = s.find(',')
+    itId = s.find("id=")
+    i = 0
+    for i in range(1):
+        itFind = s.find(',', itFind + 1)
+    moveId = s[itId + 3:itFind]
+    # print(moveId)
+    return moveId
+
+def dumpMoves(pokeName, moves):
+    print(pokeName, "has", str(len(moves)), "moves.")
+    print("ID:", "Name:")
+    for key in moves:
+        print(key, "->", moves[key])
+
+
 
 # You can only access api through these two lines. Treat this as the main function.
 async def main() -> None:
     async with aiopoke.AiopokeClient() as client:
-        moveSet = set()
-        search = 'pikachu'
-        pokemon = await client.get_pokemon(search)
+        pokemonOneMoves = {}
+        pokemonTwoMoves = {}
+        # search = 'pikachu'
+        # pokemon = await client.get_pokemon(search)
         # ability = await client.get_ability(search)
         # ability = await client.get_berry_firmness("chesto")
         # print(ability)
         # json.loads(pokemon.moves)
-        print("Pokemon: " + pokemon.name)
-        print("Pokemon moves: ")
-        loadMoves(pokemon.name, pokemon.moves, moveSet)
+        opponentOne = input("Enter pokemon 1: ")
+        opponentTwo  = input("Enter pokemon 2: ")
+        opponentOne = opponentOne.lower()
+        opponentTwo = opponentTwo.lower()
 
+        pokemonOne = await client.get_pokemon(opponentOne)
+        pokemonTwo = await client.get_pokemon(opponentTwo)        
+
+        loadMoves(pokemonOne.name, pokemonOne.moves, pokemonOneMoves)
+        loadMoves(pokemonTwo.name, pokemonTwo.moves, pokemonTwoMoves)
+
+
+        # Search moves' base damage | Make another dict with the id and the move's base damage?
+        # Develop type chart matrix, function, and txt file to read from.
+        # Search moves on type chart in comparison against opposing pokemon.
+            # sort move effectiveness in a max and min heap for each pokemon. (2 min and 2 max)
+        
+        print("Pokemon 1: " + pokemonOne.name)
+        print("Pokemon 1 moves: ")
+        dumpMoves(pokemonOne.name, pokemonOneMoves)
+        
+        print("Pokemon 2: " + pokemonTwo.name)
+        print("Pokemon 2 moves: ")
+        dumpMoves(pokemonTwo.name, pokemonTwoMoves)
+
+        
+    
 asyncio.run(main())
