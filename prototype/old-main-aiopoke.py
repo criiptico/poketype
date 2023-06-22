51545591826ca8b312ad8f1ebe31010e23307436
Adrian Quiroz <adrianquiroz166@gmail.com>
2023-06-22 10:08:28 -0500

    The bug is on lines 61 - 66.

--- prototype/old-main-aiopoke.py
+++ prototype/old-main-aiopoke.py
@@ -1,6 +1,3 @@
-# print("Hello World!")
-
-
 # Test code provided by AioPokeApi at https://beastmatser.github.io/aiopoke/
 # Running this code to test how it works.
 
@@ -24,40 +21,35 @@ import aiopoke
 #         pokemon = await client.get_pokemon('pikachu')
 #         print(pokemon.name)
 
-    
 
-
-# The input should be everything after PokemonMove and up to the next PokemonMove or a \n
-def loadMoves(pokemon, moves, moveMap): # What data type is moves??? Is moves a list?
+def loadMoves(pokemon, moves, moveMap):
     count = 0
     for i in moves: 
         currentMove = str(i)
         moveIt = str(i).find(',')
         findMoves(str(i), ',', 3, moveMap)
-        # print(str(i))
     return moveMap
 
 def findMoves(s, toFind, n, moveMap):
     itFind = s.find(toFind)
     itName = s.find("name=") + 6
-    # moveMap = {}
     i = 0
+
     for i in range(n):
         itFind = s.find(toFind, itFind + 1)
     moveName = s[itName:itFind - 2]
     moveMap[findId(s)] = moveName
-    # print(s[itName:itFind - 2])
-    # moveSet[] = moveName
+
     return moveMap
 
 def findId(s):
     itFind = s.find(',')
     itId = s.find("id=")
     i = 0
+
     for i in range(1):
         itFind = s.find(',', itFind + 1)
     moveId = s[itId + 3:itFind]
-    # print(moveId)
     return moveId
 
 def dumpMoves(pokeName, moves):
@@ -66,48 +58,63 @@ def dumpMoves(pokeName, moves):
     for key in moves:
         print(key, "->", moves[key])
 
-
+async def loadBaseDamage(pokemon, moves):
+    async with aiopoke.AiopokeClient() as client:
+        print("Displaying the damage of each move: ")
+        for move in moves.keys():
+            moveData = await client.get_move(move)
+            print(pokemon.name, ":", move, "->", moveData.power)
+            
 
 # You can only access api through these two lines. Treat this as the main function.
 async def main():
     async with aiopoke.AiopokeClient() as client:
-        pokemonOneMoves = {}
-        pokemonTwoMoves = {}
+        # Available moves each opponent has
+        opponentOneMoves = {}
+        opponentTwoMoves = {}
+        
+        # Store the base damage of each move for both opponents
+        baseOne = {}
+        baseTwo = {}
+        
 
-        # search = 'pikachu'
-        # pokemon = await client.get_pokemon(search)
-        # ability = await client.get_ability(search)
-        # berry_firmness = await client.get_berry_firmness(1)
-        # print(pokemon.name)
-        # print(berry_firmness.name)
+        pokemonOne = input("Enter pokemon 1: ")
+        pokemonTwo  = input("Enter pokemon 2: ")
+        pokemonOne = pokemonOne.lower()
+        pokemonTwo = pokemonTwo.lower()
 
-        opponentOne = input("Enter pokemon 1: ")
-        opponentTwo  = input("Enter pokemon 2: ")
-        # opponentOne = 1
-        # opponentTwo = 2
-        # opponentOne = opponentOne.lower()
+        opponentOne = await client.get_pokemon(pokemonOne)
+        loadMoves(opponentOne.name, opponentOne.moves, opponentOneMoves)
 
-        # ability1 = await client.get_ability(opponentOne)
-        # print(ability1.name)
+        opponentTwo = await client.get_pokemon(pokemonTwo)
+        loadMoves(opponentTwo.name, opponentTwo.moves, opponentTwoMoves)
 
-        pokemonOne = await client.get_pokemon(opponentOne)
-        loadMoves(pokemonOne.name, pokemonOne.moves, pokemonOneMoves)
+        await loadBaseDamage(opponentOne, opponentOneMoves)
+        await loadBaseDamage(opponentTwo, opponentTwoMoves)
 
-        pokemonTwo = await client.get_pokemon(opponentTwo)
-        loadMoves(pokemonTwo.name, pokemonTwo.moves, pokemonTwoMoves)
+
+        # Note: Make a min and max heap for each pokemon with respect to their opposing pokemon
+        ## The idea is to make a min and max of moves that are most effective for each pokemon
+        ## against the pokemon they're against.
+        oneMin = [None] * len(opponentOneMoves)
+        oneMax = [None] * len(opponentOneMoves)
+        twoMin = [None] * len(opponentTwoMoves)
+        twoMax = [None] * len(opponentTwoMoves)
+
 
         # Search moves' base damage | Make another dict with the id and the move's base damage?
         # Develop type chart matrix, function, and txt file to read from.
         # Search moves on type chart in comparison against opposing pokemon.
             # sort move effectiveness in a max and min heap for each pokemon. (2 min and 2 max)
         
-        print("Pokemon 1: " + pokemonOne.name)
+        print("Pokemon 1: " + opponentOne.name)
         print("Pokemon 1 moves: ")
-        dumpMoves(pokemonOne.name, pokemonOneMoves)
+        dumpMoves(opponentOne.name, opponentOneMoves)
+        print()
         
-        print("Pokemon 2: " + pokemonTwo.name)
+        print("Pokemon 2: " + opponentTwo.name)
         print("Pokemon 2 moves: ")
-        dumpMoves(pokemonTwo.name, pokemonTwoMoves)
+        dumpMoves(opponentTwo.name, opponentTwoMoves)
 
         
     
